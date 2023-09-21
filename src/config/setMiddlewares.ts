e67e295f25be1e503a25025d56e7f7382a7ca07e
Luis <luismanlaz@hotmail..com>
2023-09-21 18:11:44 -0300

    21/09/2023

--- src/config/setMiddlewares.ts
+++ src/config/setMiddlewares.ts
@@ -1,27 +1,19 @@
-import express, { NextFunction } from "express";
+import { Request, Response, NextFunction, Express, json } from "express";
 import morgan from "morgan";
 import { resError } from "../utils";
 
-const setMiddlewares = (app: express.Express) => {
-  app.use(express.json());
+const setMiddlewares = (app: Express) => {
+  app.use(json());
   app.use(morgan("dev"));
 
-  app.use(
-    (
-      err: Error,
-      req: express.Request,
-      res: express.Response,
-      next: NextFunction
-    ) => {
-      // Establece el código de estado en la respuesta
-      res.status(400); // Puedes definir un código de estado adecuado aquí
-
-      const message = err.message || "Internal Server Error"; // Mensaje por defecto
-      resError(res, res.statusCode, message);
-
-      console.log(err);
-    }
-  );
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    // Establece el código de estado en la respuesta
+    res.status(400); // Puedes definir un código de estado adecuado aquí
+    console.log("ZZZ");
+    const message = err.message || "Internal Server Error"; // Mensaje por defecto
+    // Utiliza resError para enviar una respuesta personalizada
+    resError(res, res.statusCode, message);
+  });
 };
 
 export default setMiddlewares;
