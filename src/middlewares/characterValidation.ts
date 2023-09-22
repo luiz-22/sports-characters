ea575a332ca1009e7a99801ba07a54c4be7ec3a3
Luis <luismanlaz@hotmail..com>
2023-09-22 12:48:20 -0300

    22/09/2023

--- src/middlewares/characterValidation.ts
+++ src/middlewares/characterValidation.ts
@@ -14,10 +14,76 @@ const initValidation = (req: Request, res: Response, next: NextFunction) => {
   next();
 };
 
-const validateUsername = (req: Request, res: Response, next: NextFunction) => {
-  const { username } = req.body;
-  if (!username) req.errors.push("Falta el username");
-  if (username && username.length < 6)
-    req.errors.push("El username debe tener un tamaño de 6 caracteres mínimo");
+// const validateId = (req: Request, res: Response, next: NextFunction) => {
+//   const { id } = req.body;
+//   if (!id) req.errors.push("The id is missing.");
+//   next();
+// };
+
+const validateName = (req: Request, res: Response, next: NextFunction) => {
+  const { name } = req.body;
+  if (!name) req.errors.push("The name is missing.");
   next();
+};
+
+const validateGender = (req: Request, res: Response, next: NextFunction) => {
+  const { gender } = req.body;
+  if (!gender) req.errors.push("The gender is missing.");
+  if (gender && (gender !== "Male" || gender !== "Female"))
+    req.errors.push("Gender must be Male or Female.");
+  next();
+};
+
+const validateAge = (req: Request, res: Response, next: NextFunction) => {
+  const { age } = req.body;
+  if (!age) req.errors.push("The age is missing.");
+  if (parseInt(age) < 1) req.errors.push("The age must be greater than zero.");
+  next();
+};
+
+const validateHeight = (req: Request, res: Response, next: NextFunction) => {
+  const { height } = req.body;
+  if (!height) req.errors.push("The height is missing.");
+  if (parseInt(height) < 1)
+    req.errors.push("The height must be greater than zero.");
+  next();
+};
+
+const validateImage = (req: Request, res: Response, next: NextFunction) => {
+  const { image } = req.body;
+  if (!image) req.errors.push("The image is missing.");
+  next();
+};
+
+const validateCountry = (req: Request, res: Response, next: NextFunction) => {
+  const { country } = req.body;
+  if (!country) req.errors.push("The country is missing.");
+  next();
+};
+
+const validateSports = (req: Request, res: Response, next: NextFunction) => {
+  const { sport } = req.body;
+  if (!sport) req.errors.push("The sport is missing.");
+  next();
+};
+
+const validateErrors = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.errors);
+  if (req.errors.length) {
+    res.status(400).json(req.errors);
+  } else {
+    next();
+  }
+};
+
+export {
+  initValidation,
+  validateName,
+  validateGender,
+  validateAge,
+  validateHeight,
+  validateImage,
+  validateCountry,
+  validateSports,
+  validateErrors,
 };
