b5bfb589859cc4674229f74b664b144550b9d447
Luis <luismanlaz@hotmail..com>
2023-09-23 11:55:58 -0300

    23/09/2023

--- src/middlewares/characterValidation.ts
+++ src/middlewares/characterValidation.ts
@@ -1,96 +1,37 @@
 import { Request, Response, NextFunction } from "express";
+import { validationResult, check } from "express-validator";
 
-// Define un tipo personalizado que extiende Request para incluir la propiedad 'errors'
-declare global {
-  namespace Express {
-    interface Request {
-      errors: string[];
+const characterValidation = [
+  check("name").notEmpty().withMessage("The name is missing."),
+  check("gender")
+    .notEmpty()
+    .withMessage("The gender is missing.")
+    .isIn(["Male", "Female"])
+    .withMessage("Gender must be Male or Female."),
+  check("age")
+    .notEmpty()
+    .withMessage("The age is missing.")
+    .isInt({ min: 1 })
+    .withMessage("The age must be greater than zero."),
+  check("height")
+    .notEmpty()
+    .withMessage("The height is missing.")
+    .isFloat({ min: 0.01 })
+    .withMessage("The height must be greater than 0."),
+  check("image").notEmpty().withMessage("The image is missing."),
+  check("country").notEmpty().withMessage("The country is missing."),
+  check("sports")
+    .notEmpty()
+    .withMessage("The sport is missing.")
+    .isArray()
+    .withMessage("Sports must be an array."),
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
     }
-  }
-}
-
-const initValidation = (req: Request, res: Response, next: NextFunction) => {
-  req.errors = [];
-  next();
-};
-
-const validateName = (req: Request, res: Response, next: NextFunction) => {
-  const { name } = req.body;
-  if (!name) req.errors.push("The name is missing.");
-  next();
-};
-
-const validateGender = (req: Request, res: Response, next: NextFunction) => {
-  const { gender } = req.body;  
-  if (!gender) req.errors.push("The gender is missing.");
-  if (gender && (gender !== "Male" && gender !== "Female"))
-    req.errors.push("Gender must be Male or Female.");
-  next();
-};
-
-const validateAge = (req: Request, res: Response, next: NextFunction) => {
-  const { age } = req.body;
-  if (!age) req.errors.push("The age is missing.");
-  if (parseInt(age) < 1) req.errors.push("The age must be greater than zero.");
-  next();
-};
-
-const validateHeight = (req: Request, res: Response, next: NextFunction) => {
-  const { height } = req.body;
-  if (!height) req.errors.push("The height is missing.");
-  if (parseInt(height) > 0)
-    req.errors.push("The height must be greater than zero.");
-  next();
-};
-
-const validateImage = (req: Request, res: Response, next: NextFunction) => {
-  const { image } = req.body;
-  if (!image) req.errors.push("The image is missing.");
-  next();
-};
-
-const validateCountry = (req: Request, res: Response, next: NextFunction) => {
-  const { country } = req.body;
-  if (!country) req.errors.push("The country is missing.");
-  next();
-};
-
-const validateSports = (req: Request, res: Response, next: NextFunction) => {
-  const { sports } = req.body;
-  if (!sports) req.errors.push("The sport is missing.");
-  next();
-};
-
-const validateErrors = (req: Request, res: Response, next: NextFunction) => {
-  if (req.errors.length) {
-    res.status(400).json(req.errors);
-  } else {
     next();
-  }
-};
-
-const characterValidation = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  initValidation(req, res, () => {
-    validateName(req, res, () => {
-      validateGender(req, res, () => {
-        validateAge(req, res, () => {
-          validateHeight(req, res, () => {
-            validateImage(req, res, () => {
-              validateCountry(req, res, () => {
-                validateSports(req, res, () => {
-                  validateErrors(req, res, next);
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-};
+  },
+];
 
 export default characterValidation;
